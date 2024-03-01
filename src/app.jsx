4c06ea17a9001d16399e81df46ff0009355d96c4
Ana-Maria Grădinaru <anamariagradinaru92@yahoo.com>
2024-03-02 00:19:14 +0200

    Added lint-staged + prettier config + fixes

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
-import Pages from './pages'
-import './styles/global.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import Pages from './pages';
+import './styles/global.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
     <ChakraProvider>
       <Pages />
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
