6472cc08869070661fc8f54f57be09806ef7c9ce
Ana-Maria Grădinaru <anamariagradinaru92@yahoo.com>
2024-03-11 14:35:00 +0200

    Fixed some issues

---
+++ src/features/roles/components/AddNewAdmin/index.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 70px;
+  border-radius: 16px;
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 60px;
+  
+`;
+
+export default Container;
